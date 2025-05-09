fa5d5071914904fdf16550fb24e1e7c378e5c24b
Nivedita <nivedita.ambulkar@ngauge.digital>
2025-05-09 15:10:39 +0530

    Modify Message

---
+++ force-app/main/default/lwc/setUpComponent/setUpComponent.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class SetUpComponent extends LightningElement {
+    showToast() {
+    const event = new ShowToastEvent({
+        title: 'Get Help',
+        message:
+            'Salesforce documentation is available ',
+    });
+    this.dispatchEvent(event);
+}}
